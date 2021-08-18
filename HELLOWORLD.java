fb2f642a7b70cbb4a06ebd8fdc0da7481acedca0
saurabh <dekapurwars@gmail.com>
2021-08-18 17:18:13 +0530

    first version

---
+++ HELLOWORLD.java
@@ -0,0 +1,15 @@
+public class HELLOWORLD{
+public static void main(String args[])
+ {
+    while(true){
+   System.out.println("HELLO WORLD");
+    try {
+       Thread.sleep(60000);
+}
+     catch(Exception ex)
+    {System.out.println(ex);
+}
+}
+}
+}
+
